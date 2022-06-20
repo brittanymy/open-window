fb0d03d4fa7ccaea1189a6621e45a77ef7268b4a
brittanymy <brittany.young2001@gmail.com>
2022-06-20 10:48:06 -0400

    updated home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
-const { Post, Comment, User } = require('../models/');
+const { Post, Comment, User } = require('../models');
 
-// get all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    // we need to get all Posts and include the User for each (change lines 8 and 9)
-    const postData = await SomeModel.someSequelizeMethod({
-      include: [SomeOtherModel],
+    const postData = await Post.findAll({
+      attributes: ["id", "title", "body", "createdAt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        }
+      ],
     });
-    // serialize the data
+
     const posts = postData.map((post) => post.get({ plain: true }));
-    // we should render all the posts here
-    res.render('hmmmm what view should we render?', { posts });
+
+    res.render("all-posts", { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -22,7 +30,7 @@ router.get('/post/:id', async (req, res) => {
   try {
     // what should we pass here? we need to get some data passed via the request body (something.something.id?)
     // change the model below, but not the findByPk method.
-    const postData = await SomeModel.findByPk(????, {
+    const postData = await SomeModel.findByPk(req.params.id, {
       // helping you out with the include here, no changes necessary
       include: [
         User,
@@ -37,7 +45,7 @@ router.get('/post/:id', async (req, res) => {
       // serialize the data
       const post = postData.get({ plain: true });
       // which view should we render for a single-post?
-      res.render('hmmmm what view should we render?', { post });
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
     } else {
       res.status(404).end();
     }
@@ -46,7 +54,6 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// giving you the login and signup route pieces below, no changes needed.
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
