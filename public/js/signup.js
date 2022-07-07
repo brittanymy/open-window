2079ebc1be4303751bc79e944693bc1b9a8dc827
brittanymy <brittany.young2001@gmail.com>
2022-07-07 12:19:15 -0400

    added email stuff

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,15 @@
 const signupFormHandler = async function(event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-signup');
-  const passwordEl = document.querySelector('#password-signup');
-  
-  const response = await fetch('/api/user', {
+  const usernameEl = document.querySelector('#username-input-signup');
+  const passwordEl = document.querySelector('#password-input-signup');
+  const emailEl = document.querySelector('#email-input-signup');
+
+  const response = await fetch('/api/user/', {
     method: 'POST',
     body: JSON.stringify({
       username: usernameEl.value,
+      email: emailEl.value,
       password: passwordEl.value,
     }),
     headers: { 'Content-Type': 'application/json' },
